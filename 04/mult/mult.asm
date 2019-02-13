4833b800bdcb63eaafd25e5d339416c9a250ccb3
Marji Symonds <symondsm@oregonstate.edu>
2019-02-13 15:38:15 -0800

    mult added

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,29 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+   
+	@R2
+	M=0
+
+ (LOOP)
+	@R1
+	D=M
+
+	@END
+	D;JEQ	//If R1=0 go to end
+
+	@R0
+	D=M	//Set D to R0's value 
+
+	@R2
+	M=M+D	//Add R0's value to R2
+	
+	@R1
+	M=M-1	//Decrement R1
+
+	@LOOP
+	0; JMP	//go to loop	
+
+    (END)
+	@END
+	0;JMP	//infinite loop
