2a966663179cafd4e005927f03ec5b6c11f3a095
Marji Symonds <symondsm@oregonstate.edu>
2019-02-18 12:48:21 -0800

    fill added

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,58 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(START)
+ 	@8192
+ 	D=A
+
+	@i 	//set counter to cycle through
+	M=D
+
+	@current 	//set address adder to 0
+	M=0
+
+(INPUT)
+	@KBD
+	D=M 	//get input from keyboard
+
+	@LOOP2
+	D; JEQ 	//no key press=white
+
+(LOOP)
+	@current 	//get address adder
+	D=M
+
+	@SCREEN 	//go to current pixel & set black
+	A=A+D
+	M=-1
+
+	@END
+	0; JMP	//go to end to increment/decrement
+
+(LOOP2)
+	@current 	//get address adder
+	D=M
+
+	@SCREEN 	//go to current pixel & set white
+	A=A+D
+	M=0
+
+	@END
+	0; JMP	//go to end to increment/decrement
+
+(END)
+	@current
+	M=M+1	//increment adder go to next screen address
+
+	@i
+	M=M-1	//Decrement screen location count
+
+	@i
+	D=M
+
+	@START
+	D; JEQ	//If at the end of the screen mem, start over
+
+	@INPUT 	//otherwise get next key input
+	0; JMP
+
